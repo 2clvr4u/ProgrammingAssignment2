d21f9687d0f2c29410553f7d1f63e96ad62df06e
2clvr4u <78439853+2clvr4u@users.noreply.github.com>
2021-04-04 19:21:31 -0400

    Update makeCacheMatrix.r

--- makeCacheMatrix.r
+++ makeCacheMatrix.r
@@ -1,5 +1,12 @@
 makeCacheMatrix <- function(x = matrix()) 
 {
+  ## this function creates a matrix object that can cache its own inverse.
+  ## the following is done
+  ## set the value of the matrix
+  ## get the value of the matrix
+  ## set the value of the inverse
+  ## get the value of the inverse
+  
   m <- NULL
   set <- function(y) {
     x <<- y
